c922b34f733708b38decd73dc9c09a8188f11817
Hiroki Ueno <rockyueno@gmail.com>
2024-07-25 16:36:52 -0700

    added tyr-catch

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 const weatherWrapper = document.querySelector('.weather-wrapper')
 
 async function getWeather() {
-  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&current=temperature_2m,is_day,rain,showers,wind_speed_10m&timezone=auto&forecast_days=1')
-  const data = await response.json()
+  try {
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&current=temperature_2m,is_day,rain,showers,wind_speed_10m&timezone=auto&forecast_days=1')
+    const data = await response.json()
 
-  const date = new Date(data.current.time).toLocaleString()
+    const date = new Date(data.current.time).toLocaleString()
 
-  let weatherElement = document.createElement('div')
-  weatherElement.innerHTML = `<p>Today's Weather</p><p class='temperature'>${data.current.temperature_2m} ${data.current_units.temperature_2m}</p><p>Wind Speed: ${data.current.wind_speed_10m} ${data.current_units.wind_speed_10m}</p><p>${data.timezone}</p><P>Last updated: ${date}</P>`
-  weatherWrapper.append(weatherElement)
+    let weatherElement = document.createElement('div')
+    weatherElement.innerHTML = `<p>Today's Weather</p><p class='temperature'>${data.current.temperature_2m} ${data.current_units.temperature_2m}</p><p>Wind Speed: ${data.current.wind_speed_10m} ${data.current_units.wind_speed_10m}</p><p>${data.timezone}</p><P>Last updated: ${date}</P>`
 
+    weatherWrapper.append(weatherElement)
+
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getWeather()
